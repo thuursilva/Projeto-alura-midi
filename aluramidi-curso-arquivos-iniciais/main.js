782328ddb816add18688328d71d55bc734e27027
Arthur <thurfsilva@gmail.com>
2023-05-24 21:30:33 -0300

    Iniciando comandos simples de JS

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,4 @@
+//selecionando um elemento do arquivo HTML
+//document = referencia a origem do documento (arquivo HTML)
+//querySelector = busca o elemento, sempre em aspas simples
+document.querySelector('.tecla_pom');
